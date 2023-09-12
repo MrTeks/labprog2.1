c0153bd79a6048409f9bae98f55e6b9209a5be20
Teks <Teks@TEKS>
2023-09-12 17:51:24 +0700

    Добавьте файлы проекта.

---
+++ labprog2.1/labprog2.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    double a, b, c;
+
+    cout << "Введите сторону a: ";
+    cin >> a;
+    cout << "Введите сторону b: ";
+    cin >> b;
+    cout << "Введите сторону c: ";
+    cin >> c;
+
+    double v = a * (b * c);
+    double s = 2 * ((a * b) + (b * c) + (a * c));
+
+    cout << "Объем параллелепипеда: " << v << endl;
+    cout << "Площадь поверхности параллелепипеда: " << s << endl;
+}
